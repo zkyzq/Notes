5b65dde9abcef686edf4bb6bc2774c0caf6794cf
yufengwhy <724193552@qq.com>
2017-07-26 14:31:08 +0800

    Update DL_in_NLP.py

--- DL_in_NLP.py
+++ DL_in_NLP.py
@@ -14,14 +14,16 @@ model = BoWClassifier(NUM_LABELS, VOCAB_SIZE)
 log_probs = model(autograd.Variable(bow_vector))  #how to pass the parameter?
 '''
 
-#answer is as the following steps
+#answers as follows
 #----------------   1    ----------------#
 # __init__ and __call__ in python
+
 #1.1 __init__ in python
 class foo:
     def __init__(self, a, b, c):
         # ...
 x = foo(1, 2, 3) # __init__
+
 #1.2 __call__ in python
 class foo:
     def __call__(self, a, b, c):
@@ -29,32 +31,37 @@ class foo:
 x = foo()
 x(1, 2, 3) # __call__
 
-'2.  Python中函数的参数定义和可变参数
-# http://blog.csdn.net/feisan/article/details/1729905
+#----------------   2    ----------------#
+# Python中函数的参数定义和可变参数: http://blog.csdn.net/feisan/article/details/1729905
 # func(*args, **kwargs)
-# 带*的参数就是用来接受可变数量个参数
+
+#2.1 带*的参数就是用来接受可变数量个参数
 def funcD(a, b, *c):
   print a
   print b
   print c
-调用funcD(1, 2, 3, 4, 5, 6)结果是
+'''调用funcD(1, 2, 3, 4, 5, 6)结果是
 1
 2
-(3, 4, 5, 6)
-# 带**的参数就是用来接受一个字典
+(3, 4, 5, 6)'''
+
+#2.2 带**的参数就是用来接受一个字典
 def funcF(a, **b):
   print a
   for x in b:
     print x + ": " + str(b[x])
-调用funcF(100, c='你好', b=200)，执行结果
+'''调用funcF(100, c='你好', b=200)，执行结果
 100
 c: 你好
-b: 200
+b: 200'''
 
-'3. python class
-class BoWClassifier(nn.Module):  # inheriting from nn.Module!
+#----------------   3    ----------------#
 
-'4. pytorch in nn.Module
+# nn.Module in pytorch
+# a new nn.Module will inherit the basic nn.Module in pytorch
+# the new nn.Module should override the func. forward() 
+# the new nn.Module will inherit the func. __call__(), so when we call
+# the new nn.Module, we can directly pass parameters of input to func. forward()
 def __call__(self, *input, **kwargs):
         result = self.forward(*input, **kwargs)
 
