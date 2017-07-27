81f9861ee89fdc574f483fdc1d353612bd3ae804
yufengwhy <724193552@qq.com>
2017-07-27 16:30:54 +0800

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -7,10 +7,11 @@ inputs = [autograd.Variable(torch.randn((1, 3)))
           for _ in range(5)]  # make a sequence of length 5
 
 # initialize the hidden state. 
-# hidden = (hidden_state, cell_state)
+# hidden = (hidden_n, cell_n) # state at time n
 hidden = (autograd.Variable(torch.randn(1, 1, 3)),
           autograd.Variable(torch.randn((1, 1, 3))))
 for i in inputs:
     # Step through the sequence one element at a time.
     # after each step, hidden contains the hidden state.
+    # out (seq_len, batch, hidden_size)
     out, hidden = lstm(i.view(1, 1, -1), hidden)
