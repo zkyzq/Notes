02fc56a9a5322498cc3fcbc2448a82034a83eb4a
yufengwhy <724193552@qq.com>
2017-07-27 17:19:22 +0800

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,15 +1,19 @@
 # http://pytorch.org/docs/master/_modules/torch/nn/modules/rnn.html#LSTM
 # http://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
-# Pytorch’s LSTM expects all of its inputs to be 3D tensors: sequence_len, mini-batch, input_dim
 
-lstm = nn.LSTM(3, 3, 2)  # Input dim is 3, hidden dim is 3, # recurrent layers is 2
-inputs = [autograd.Variable(torch.randn((1, 3)))
+# input_dim, hidden_dim, num_layers = (3, 3, 2)
+lstm = nn.LSTM(3, 3, 2)  
+
+# inputs is 3D tensors: sequence_len, minibatch_size, input_dim
+inputs = [autograd.Variable(torch.randn((1, 3))) 
           for _ in range(5)]  # make a sequence of length 5
 
 # initialize the hidden state. 
 # hidden = (hidden_n, cell_n) # state at time n
+# state is 3D tensors: num_layers, minibatch_size, hidden_dim
 hidden = (autograd.Variable(torch.randn(1, 1, 3)),
           autograd.Variable(torch.randn((1, 1, 3))))
+
 for i in inputs:
     # Step through the sequence one element at a time.
     # after each step, hidden contains the hidden state.
