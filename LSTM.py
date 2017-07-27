dbbbe62ebad181506d1e1f0cde1968cceb71cd47
yufengwhy <724193552@qq.com>
2017-07-27 17:38:32 +0800

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,6 +1,9 @@
 # http://pytorch.org/docs/master/_modules/torch/nn/modules/rnn.html#LSTM
 # http://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
 
+# ------------- 1 ------------- #
+# meaning of para. in ()
+
 # para.: input_dim, hidden_dim, num_layers 
 lstm = nn.LSTM(3, 3, 2)  
 
@@ -19,3 +22,17 @@ for i in inputs:
     # after each step, hidden contains the hidden state.
     # out (seq_len, batch, hidden_size)
     out, hidden = lstm(i.view(1, 1, -1), hidden)
+          
+# ------------- 2 ------------- #
+# remember to clear out the hidden& cell state of the LSTM
+# before each instance
+
+for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+    for sentence, tags in training_data:
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Also, we need to clear out the hidden state of the LSTM,
+        # detaching it from its history on the last instance.
+        model.hidden = model.init_hidden()
